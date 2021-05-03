abb0b5beb50dff47df4017f551e77de22ccbf871
Eclipse <81539816+Eclipse143@users.noreply.github.com>
2021-05-02 18:33:14 -0700

    I made more changes

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -4,24 +4,43 @@ using UnityEngine;
 
 public class UI : MonoBehaviour
 {
-    public GameObject Instructions;
+    public GameObject Instructions1;
+    public GameObject Instructions2;
+    public GameObject Instructions3;
+    public GameObject tilemap;
+    public IEnumerator coroutine;
 
     void Start()
     {
 	if(PlayerData.score == 0)
 	{
-	    StartCoroutine("TurnOff");
+	    coroutine = TurnOff(Instructions1);
+	    StartCoroutine(coroutine);
 	}
 	else
 	{
-	    Instructions.SetActive(false);
+	    Instructions1.SetActive(false);
+	}
+
+	if(PlayerData.score == 1000)
+	{
+	    Instructions2.SetActive(true);
+	    coroutine = TurnOff(Instructions2);
+	    StartCoroutine(coroutine);
+	    Debug.Log("coroutine ended");
 	}
     }
 
-    IEnumerator TurnOff()
+    public void leave()
     {
-        yield return new WaitForSeconds(5f);
-	Instructions.SetActive(false);
+	Instructions3.SetActive(true);
+	tilemap.SetActive(true);
+    }
+
+    IEnumerator TurnOff(GameObject instr)
+    {
+	yield return new WaitForSecondsRealtime(3f);
+	instr.SetActive(false);
 	Time.timeScale = 1f;
     }
 }
